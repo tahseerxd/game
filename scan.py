7993962a38243795dbce3784876c139f045b25ea
Tabish M <110502581+tahseerxd@users.noreply.github.com>
2023-03-05 00:43:58 +0500

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,74 +1,74 @@
 import socket
-import sys
 import os
 
-# Define colors for output
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
+# Clear the terminal screen
+os.system('clear')
 
-# Define dictionary of game ports and their associated games
-game_ports = {
-    80: 'Quake 3',
-    6112: 'Diablo 2',
-    7777: 'Unreal Tournament',
-    8888: 'Unreal Tournament',
-    27960: 'Return to Castle Wolfenstein',
-    20560: 'Aliens vs. Predator',
-    20561: 'Aliens vs. Predator',
-    8767: 'TeamSpeak',
-    27015: 'Half-Life',
-    27016: 'Half-Life',
-    27017: 'Half-Life',
-    27018: 'Half-Life',
-    27019: 'Half-Life',
-    28960: 'Call of Duty',
-    29900: 'Steam',
-    29901: 'Steam',
-    29920: 'Steam',
-    17475: 'Minecraft',
-    25565: 'Minecraft'
-}
+# Define color codes for printing to the terminal
+GREEN = '\033[32m'
+RED = '\033[31m'
+YELLOW = '\033[33m'
+RESET = '\033[0m'
 
-# Clear screen and print header
-os.system('cls' if os.name == 'nt' else 'clear')
-print(bcolors.HEADER + '------------------------------------------------------')
-print('            GAME PORT SCANNER FOR HOSTS')
-print('------------------------------------------------------\n' + bcolors.ENDC)
+# Print welcome message and options to the terminal
+print(f"{GREEN}{'='*30}\nWelcome to the Port Scanner!\n{'='*30}{RESET}\n")
+print(f"{YELLOW}Please select the type of port scan you'd like to perform:\n{'-'*60}")
+print(f"1) TCP Ports\n2) UDP Ports\n3) Gaming Ports\n{'-'*60}{RESET}")
 
-# Ask user for target host or IP
-target_host = input('Enter target host or IP address: ')
+# Get user input for the type of port scan to perform
+scan_type = input(f"{YELLOW}Enter the number of the scan type you'd like to perform: {RESET}")
 
-# Create a TCP socket object
-tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_sock.settimeout(1.0)
+# Get the target IP address from the user
+target_host = input(f"{YELLOW}Enter the IP address you'd like to scan: {RESET}")
 
-# Create a UDP socket object
-udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_sock.settimeout(1.0)
+# Define the port ranges to scan based on the user's selected scan type
+if scan_type == '1':  # TCP Ports
+    port_range = range(1, 65536)
+    scan_name = 'TCP'
+elif scan_type == '2':  # UDP Ports
+    port_range = range(1, 65536)
+    scan_name = 'UDP'
+elif scan_type == '3':  # Gaming Ports
+    port_range = [22, 80, 443, 27015, 27016, 27017, 27018, 27019, 28015, 28016, 28017, 28018, 28019, 28020]
+    scan_name = 'Gaming'
 
-# Loop through game ports and check if they're open
-for port, game in game_ports.items():
+# Clear the terminal screen
+os.system('clear')
+
+# Print the scan name and target IP address to the terminal
+print(f"{GREEN}{'='*30}\n{scan_name} Port Scan Results for {target_host}\n{'='*30}{RESET}\n")
+
+# Loop over the port range and attempt to connect to each port on the target IP address
+for port in port_range:
     try:
-        # Check if TCP port is open
-        tcp_result = tcp_sock.connect_ex((target_host, port))
-        if tcp_result == 0:
-            print(f'{bcolors.OKGREEN}[+] Port {port} ({game}) is open.{bcolors.ENDC}')
-
-        # Check if UDP port is open
-        udp_sock.sendto(b'hello', (target_host, port))
-        udp_result = udp_sock.recvfrom(1024)
-        if udp_result:
-            print(f'{bcolors.OKGREEN}[+] Port {port} ({game}) is open.{bcolors.ENDC}')
-
-    except:
-        # Print error if any exception is raised
-        print(f'{bcolors.FAIL}[-] Port {port} ({game}) is closed.{bcolors.ENDC}')
-
-# Close sockets
-tcp_sock.close()
-udp_sock.close()
+        # Create a new socket and attempt to connect to the target IP address on the current port
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM if scan_type == '1' else socket.SOCK_DGRAM)
+        sock.settimeout(1)
+        result = sock.connect_ex((target_host, port))
+        
+        # Print the port status and name to the terminal based on whether it's open or closed
+        if result == 0:
+            print(f"{GREEN}[+] {port}: Open - {get_service_name(port)}{RESET}")
+        else:
+            print(f"{RED}[-] {port}: Closed{RESET}")
+        
+        # Close the socket connection
+        sock.close()
+    except KeyboardInterrupt:
+        print(f"\n{YELLOW}[*] User requested exit. Exiting...{RESET}")
+        exit()
+    except socket.gaierror:
+        print(f"{RED}[-] Hostname could not be resolved. Exiting...{RESET}")
+        exit()
+    except socket.error:
+        print(f"{RED}[-] Couldn't connect to server. Exiting...{RESET}")
+        exit()
+        
+def get_service_name(port):
+    if port == 22:
+        return "SSH"
+    elif port == 80 or port == 8080:
+        return "HTTP"
+    elif port == 443:
+        return "HTTPS"
+    elif port == 27015 or port == 27016 or port == 27017
